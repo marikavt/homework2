b9651a95e3aefba32e1c11cc90dff06a94c4f520
mariami <mariam.kavtaradze978@gmail.com>
2023-03-07 22:54:50 +0400

    add file

---
+++ script.js
@@ -0,0 +1,50 @@
+
+// const array1 = [{name: 'Temo', age:25}, {name: 'Lasha', age:21}, {name: 'Ana', age:28}]
+
+// function user_name (array1){
+//     let max_age=array1[0].age;
+//     console.log(max_age);
+//     let ag_e;
+//     for (let the of array1){
+//         if (the.age<max_age){
+//             max_age=the.age;
+//             ag_e=the.name;
+//         }
+//     }
+//     return ag_e;
+// }
+// console.log(user_name(array1))
+
+
+
+
+// const new_array=[...array1];
+// console.log(array1, new_array)
+
+
+
+
+
+
+// let a;
+// let b;
+
+
+// do{
+//     let a = Math.floor(Math.random() * 6) + 1;
+//     let b = Math.floor(Math.random() * 6) + 1;
+
+//     console.log(a, b)
+//     if (a==3){
+//         console.log('first player is winner')
+//     } else if (b==3){
+//         console.log('second player is winner')
+//     }
+//  } while( a && b == 3)
+ 
+
+
+
+
+
+
